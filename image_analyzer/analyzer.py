5941ae9bea7492483996e38997364c7017004ebf
hogesako <hogesako@ikasako.com>
2019-11-10 14:01:39 +0900

    save image

--- image_analyzer/analyzer.py
+++ image_analyzer/analyzer.py
@@ -30,21 +30,25 @@ class Analyzer:
         tweet_id = str(tweet['id'])
         image_urls = tweet['image_urls']
         for image_url in image_urls:
-            if self.img_analyze(image_url) == True:
+            if self.img_analyze(image_url, tweet_id) == True:
                 # self.sqs.retweet(tweet_id)
                 self.logger.info('tweet id: %s', str(tweet_id))
                 break
 
-    def img_analyze(self, image_url):
+    def img_analyze(self, image_url, tweet_id):
         f = io.BytesIO(urllib.request.urlopen(image_url).read())
         img = Image.open(f)
 
         faces = self.faceDetector.detect(img)
         max_match_rate = 0
+        i = 0
         for face in faces:
             predict_proba = self.faceClassifier.predict(face)
             if predict_proba < 0.01:
+                face.save("./save/" + str(tweet_id) +
+                          "_" + str(i) + ".jpg", "JPEG")
                 self.logger.info('url: %s proba: %s', image_url,
                                  str(predict_proba))
                 return True
+            i += 1
         return False
